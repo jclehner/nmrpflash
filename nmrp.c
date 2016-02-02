87b03528201722df32fc1e0ea034668013af7234
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-02-02 14:31:42 +0200

    Be less verbose while advertising

--- nmrp.c
+++ nmrp.c
@@ -341,7 +341,7 @@ int nmrp_do(struct nmrpd_args *args)
 	beg = time(NULL);
 
 	while (1) {
-		printf("\rAdvertising NMRP server on %s ... %c", args->intf,
+		printf("\rAdvertising NMRP server on interface ... %c",
 				spinner[i]);
 		fflush(stdout);
 		i = (i + 1) & 3;
